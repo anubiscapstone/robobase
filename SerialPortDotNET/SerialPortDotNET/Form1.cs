a2b9a0cb8bf20f73b53de0cf457e1ff14e87ca40
anubiscapstone <anubiscapstone@gmail.com>
2014-09-12 17:33:47 -0400

    @Author Drew Newman
    *Cleaned up Tim's Code

--- SerialPortDotNET/SerialPortDotNET/Form1.cs
+++ SerialPortDotNET/SerialPortDotNET/Form1.cs
@@ -14,6 +14,8 @@ namespace SerialPortDotNET
     {
         int BaseVal = 1500;
         public SerialPort sport;
+        public string powerOffRobot = "#0L #1L #2L #3L #4L #5L #6L #7L #8L #9L #10L #11L #12L #13L  #14 P1500 #15 P1500\r";
+        public string centerRobot = "#0 P1500 #1 P1500 #2 P1500 #3 P1500 #4 P1500 #5 P1500 #6 P1500 #7 P1500 #8 P1500 #9 P1500 #10 P1500 #11 P1500 #12 P1500 #13 P1500 #14 P1500 #15 P1500\r";
         public Form1()
         {
             InitializeComponent();
@@ -28,14 +30,14 @@ namespace SerialPortDotNET
             //Open Port
             sport.Open();
             //Center Everything
-            sport.Write("#0 P1500 #1 P1500 #2 P1500 #3 P1500 #4 P1500 #5 P1500 #6 P1500 #7 P1500 #8 P1500 #9 P1500 #10 P1500 #11 P1500 #12 P1500 #13 P1500 #14 P1500 #15 P1500\r");
+            sport.Write();
             
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             //Power Down Robot and Port
-            sport.Write("#0L #1L #2L #3L #4L #5L #6L #7L #8L #9L #10L #11L #12L #13L  #14 P1500 #15 P1500\r");
+            sport.Write(powerOffRobot);
             sport.Close();
         }
 
