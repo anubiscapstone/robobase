09a21ba05b97c65c0ea31f0c8fe5b1a1894ec580
Brains <n-orr@onu.edu>
2014-10-07 14:32:19 -0400

    @Brains: Updated from current robot code as of 10/7/2014.

--- Johnny5/main.cpp
+++ Johnny5/main.cpp
@@ -2,29 +2,43 @@
 #include <string>
 
 #include "gpio.h"
+#include "sock.h"
+#include "serialib.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-	// Get Server IP address
-	/*if (argc < 2) {
-		cerr << "Usage: " << argv[0] << " <serverIPaddress>" << endl;
-		return -1;
-	}
-	string serverIP = argv[1];*/
 
-	// Homegrown GPIO example
-	GPIO *gpio = GPIO::getInstance();
+	Sock s;
+	string reply = "";
+	s.conn("192.168.0.105", "1337");
+	//s.sendline("tacos are my past");
+	do{
+	reply = s.readline();
+	s.sendline("Command Received");
+	cout << reply;
+	}while (reply != "q");
+	s.clo();
+
+	/*
+	GPIO* gpio = GPIO::getInstance();
 	int pin = gpio->setup("P9_12", OUTPUT);
+	int pin2 = gpio->setup("P9_14", OUTPUT);
+	gpio->setValue(pin2, HIGH);
 
-	gpio->setValue(pin, HIGH);
-	cout << "HIGH" << endl;
-	
 	sleep(5);
 
-	gpio->setValue(pin, LOW);
-	cout << "LOW" << endl;
+	//add serial code here:
+	serialib* ser = new serialib();
+	ser->Open("/dev/ttyO4", 115200);
+	ser->WriteString(reply.c_str());
 
+	sleep(10);
+
+	gpio->setValue(pin2, LOW);
 	delete gpio;
+	delete ser;
+	*/
 	return 0;
+
 }
---
+++ Johnny5/sock.cpp
@@ -0,0 +1,69 @@
+#include <cstring>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <string>
+
+#include "sock.h"
+
+using namespace std;
+
+Sock::Sock() {
+	memset(&host_info, 0, sizeof(host_info));
+	host_info.ai_family = AF_UNSPEC; // can be either ipv4 or ipv6
+	host_info.ai_socktype = SOCK_STREAM; // SOCK_STREAM = TCP
+}
+
+Sock::~Sock() {
+	freeaddrinfo(host_info_list);
+	clo();
+}
+
+int Sock::conn(char *host, char *port) {
+	int status;
+	status = getaddrinfo(host, port, &host_info, &host_info_list);
+	if (status != 0) return status;
+
+	sockid = socket(host_info_list->ai_family, host_info_list->ai_socktype, host_info_list->ai_protocol);
+	if (sockid == -1) return -1;
+
+	status = connect(sockid, host_info_list->ai_addr, host_info_list->ai_addrlen);
+	if (status != 0) return status;
+
+	return 0;
+}
+
+void Sock::sendline(char *line) {
+	ssize_t bytes_sent;
+
+	do {
+		int len = strlen(line);
+		bytes_sent = send(sockid, line, len, 0);
+
+		string sl(line);
+		sl = sl.substr(bytes_sent, sl.size() - bytes_sent);
+		line = (char *)sl.c_str();
+	} while (strlen(line) > 0);
+
+	send(sockid, "\n", 1, 0); // add \n
+}
+
+string Sock::readline() {
+	ssize_t bytes_received;
+	string line = "";
+
+	while (true) {
+		char *incomming = new char[1024];
+		bytes_received = recv(sockid, incomming, 1024, 0);
+
+		if (bytes_received <= 0) return "";
+		line += incomming;
+
+		if (line.find("\n", 0) != string::npos) break;
+	}
+
+	return line.substr(0, line.size() - 1); // remove \n
+}
+
+void Sock::clo() {
+	close(sockid);
+}
